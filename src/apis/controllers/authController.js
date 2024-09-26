ef3ea918a9a0bfb07fd7e28e0c156e1d3899c670
vishal-rajpoot <vishalrajpoot540@gmail.com>
2024-09-26 14:57:39 +0530

    eslint changes

--- src/apis/controllers/authController.js
+++ src/apis/controllers/authController.js
@@ -1,60 +1,49 @@
-// import asyncHandler from "express-async-handler";
-// import User from "../../models/userModel.js";
-// import { generateToken } from "../../config/generateToken.js";
-// import { sendSuccess } from "../../utils/responseHandler.js";
-// import {
-//   AuthenticationError,
-//   BadRequestError,
-//   DuplicateDataError,
-// } from "../../utils/appErrors.js";
+import User from "../../models/userModel.js";
+import { generateToken } from "../../config/generateToken.js";
+import { sendSuccess } from "../../utils/responseHandler.js";
+import {
+  AuthenticationError,
+  DuplicateDataError,
+} from "../../utils/appErrors.js";
 
-// const registerUser = asyncHandler(async (req, res) => {
-//   const { name, email, password, pic } = req.body;
-//   if (!name || !email || !password) {
-//     throw new BadRequestError("Please enter all the fields");
-//   }
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password, role } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const Users = await User.findAll();
+    if (Users.username === username) {
+      return new DuplicateDataError("User already exists");
+    }
 
-//   const userExists = await User.findOne({ email });
-//   if (userExists) {
-//     throw new DuplicateDataError("User already exists");
-//   }
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+      role,
+    });
 
-//   const user = await User.create({
-//     name,
-//     email,
-//     password,
-//     pic,
-//   });
-//   if (user) {
-//     const data = {
-//       _id: user._id,
-//       name: user.name,
-//       email: user.email,
-//       pic: user.pic,
-//       token: generateToken(user._id),
-//     };
-//     return sendSuccess(res, data, "User register successfully");
-//   } else {
-//     throw new BadRequestError("Failed to create a user");
-//   }
-// });
+    res.status(201).json({ message: "User registered successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: "Registration failed", error });
+  }
+};
 
-// const loginUser = asyncHandler(async (req, res) => {
-//   const { email, password } = req.body;
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
 
-//   const user = await User.findOne({ email });
-//   if (user && (await user.matchPassword(password))) {
-//     const data = {
-//       _id: user._id,
-//       name: user.name,
-//       email: user.email,
-//       pic: user.pic,
-//       token: generateToken(user._id),
-//     };
-//     return sendSuccess(res, data, "Login successfully");
-//   } else {
-//     throw new AuthenticationError("Invalid email or password");
-//   }
-// });
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    const data = {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    };
+    return sendSuccess(res, data, "Login successfully");
+  } else {
+    throw new AuthenticationError("Invalid email or password");
+  }
+};
 
-// export { registerUser, loginUser };
+export { registerUser, loginUser };
